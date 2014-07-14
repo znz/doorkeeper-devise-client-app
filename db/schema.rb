fe7e7ab5e43a1757dfbdea4465f79b12d53e522a
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-14 19:24:04 +0900

    add omniauth columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140714091803) do
+ActiveRecord::Schema.define(version: 20140714102124) do
 
   create_table "users", force: true do |t|
     t.string   "email",              default: "", null: false
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 20140714091803) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
