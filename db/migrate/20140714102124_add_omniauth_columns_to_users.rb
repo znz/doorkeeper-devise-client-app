a34d1375eaea6ef49cb5ba08b487164a998403cc
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-15 22:05:14 +0900

    add provider and uid unique index

--- db/migrate/20140714102124_add_omniauth_columns_to_users.rb
+++ db/migrate/20140714102124_add_omniauth_columns_to_users.rb
@@ -3,5 +3,6 @@ class AddOmniauthColumnsToUsers < ActiveRecord::Migration
     add_column :users, :provider, :string
     add_column :users, :uid, :string
     add_column :users, :name, :string
+    add_index :users, [:provider, :uid], unique: true
   end
 end
