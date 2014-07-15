9864a379de6b9d90c92bf6f7ac75061b5b858609
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-07-15 22:08:42 +0900

    sign_in before controller test

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, :type => :controller do
+  before do
+    @user = FactoryGirl.create(:user)
+    sign_in @user
+  end
 
   describe "GET 'index'" do
     it "returns http success" do
